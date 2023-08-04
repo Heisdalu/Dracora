1f590eda1d7017d836eba0a7847e0e30fe42cb6c
divine <divineobi07@gmail.com>
2023-08-04 23:55:25 +0100

    modal is functional

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
-import React from "react";
+import React, { MouseEventHandler } from "react";
 import MainNavigation from "./MainNavigation";
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  toggle: MouseEventHandler<HTMLButtonElement>;
+}
+
+const Header: React.FC<HeaderProps> = ({ toggle }) => {
+  
   return (
-    <div className="border-1 py-[8px] sticky top-[1rem] z-[1] mb-[2rem] md:relative md:top[0] md:py-[0]">
+    <div className="bg-mainBg border-1 py-[8px] sticky top-[0rem] mb-[2rem] md:relative md:top[0] md:mt-1 md:py-[0]">
       <header>
         <nav className="flex">
           <button
+            onClick={toggle}
             aria-label="hamburger"
             className="p-0.75 bg-[#F7F7F7] rounded-[50%]"
           >
--- src/components/Modal/NavigateModal/NavigateModal.tsx
+++ src/components/Modal/NavigateModal/NavigateModal.tsx
@@ -1,41 +1,71 @@
-import React from "react";
+import React, { MouseEventHandler, SyntheticEvent, useEffect } from "react";
 import Image from "next/image";
 import NavigateHeader from "./NavigateHeader";
 import { pages, apps, charts } from "@/constants/Modal";
 
-const NavigateModal: React.FC = () => {
+interface NavigateModalProps {
+  toggle: MouseEventHandler<HTMLButtonElement | HTMLDivElement>;
+}
+
+const NavigateModal: React.FC<NavigateModalProps> = ({ toggle }) => {
   const dashboard = [{ icon: "/navIcon/ecommerce.svg", name: "ecommerce" }];
 
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+    function handleMediaQueryChange(event: { matches: any; }) {
+      if (event.matches) {
+        // This code will execute when the screen size matches the media query
+        console.log("Screen size is less than or equal to 768px");
+      } else {
+        // This code will execute when the screen size does not match the media query
+        console.log("Screen size is greater than 768px");
+      }
+    }
+    console.log(mediaQuery);
+
+    mediaQuery.addEventListener("load", handleMediaQueryChange);
+  }, []);
+
   return (
-    <div className="navigateModal bg-white shadow-m1 max-w-[250px] border-1 w-[100%] fixed top-[0] z-[2] overflow-scroll h-[100vh] overflow-x-hidden md:sticky">
-      <nav className=" flex flex-col p-[1rem]">
-        <div className="flex">
-          <h1 className="flex items-center space-x-[12px]">
-            <span>
-              <Image
-                src="/dracora_logo.png"
-                height={30}
-                width={30}
-                alt="Dracora Logo"
-              />
+    <>
+      <div className="navigateModal bg-white shadow-m1 max-w-[250px] border-1 w-[100%] fixed top-[0] z-[3] overflow-scroll h-[100vh] overflow-x-hidden md:sticky">
+        <nav className=" flex flex-col p-[1rem] relative bg-white">
+          <div className="flex">
+            <h1 className="flex items-center space-x-[12px]">
+              <span>
+                <Image
+                  src="/dracora_logo.png"
+                  height={30}
+                  width={30}
+                  alt="Dracora Logo"
+                />
+              </span>
+              <span className="text-20px font-extrabold">Dracora</span>
+            </h1>
+
+            <span
+              onClick={toggle}
+              className="ml-[auto] flex items-center justify-center md:hidden"
+            >
+              <Image src="/navIcon/exit.svg" width={20} height={20} alt="" />
             </span>
-            <span className="text-20px font-extrabold">Dracora</span>
-          </h1>
+          </div>
 
-          <span className="ml-[auto] flex items-center justify-center md:hidden">
-            <Image src="/navIcon/exit.svg" width={20} height={20} alt="" />
-          </span>
-        </div>
-
-        <div className="my-2 py-1 flex flex-col space-y-[1.3rem]">
-          <NavigateHeader title="dashboard" elements={dashboard} />
-          <NavigateHeader title="pages" elements={pages} />
-          <NavigateHeader title="apps" elements={apps} />
-          <NavigateHeader title="charts" elements={charts} />
-          <NavigateHeader title="charts" elements={charts} />
-        </div>
-      </nav>
-    </div>
+          <div className="my-2 py-1 flex flex-col space-y-[1.3rem]">
+            <NavigateHeader title="dashboard" elements={dashboard} />
+            <NavigateHeader title="pages" elements={pages} />
+            <NavigateHeader title="apps" elements={apps} />
+            <NavigateHeader title="charts" elements={charts} />
+            <NavigateHeader title="charts" elements={charts} />
+          </div>
+        </nav>
+      </div>
+      <div
+        onClick={toggle}
+        className="fixed top-[0] h-[100vh] w-100 bg-mdHidden z-[1] md:hidden"
+      ></div>
+    </>
   );
 };
 export default NavigateModal;
