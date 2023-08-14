937fd7e1b1541edbe238fa22822cfb2562ec138e
divine <divineobi07@gmail.com>
2023-08-14 21:15:12 +0100

    fix font issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,11 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  adjustFontFallback: false,
+});
 import Provider from "@/context/Provider";
 
 export default function App({ Component, pageProps }: AppProps) {
